d4aeb27965b66ab81e16d593df90026bd2851ffe
Jonathan W <jwilliams12117@gmail.com>
2022-03-04 20:19:52 -0700

    Fixed oopsie that hardcoded port to 3001

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const uniqid = require("uniqid");
 const { title } = require("process");
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
